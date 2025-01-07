c9fb8e7506a97e3a56eb22c839961ed507982274
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2025-01-08 01:08:07 +0530

    done

--- mymalloc.c
+++ mymalloc.c
@@ -1,5 +1,6 @@
 #include "mymalloc.h"
 #include <stdint.h>
+#include <stddef.h>
 
 #define MEMORY_SIZE 25000
 static unsigned char memory[MEMORY_SIZE];
@@ -35,9 +36,23 @@ void splitBlock(Block* block, size_t size) {
     newBlock->size = block->size - size - sizeof(Block);
     newBlock->next = block->next;
     newBlock->free = 1;
+
     block->size = size;
     block->next = newBlock;
     block->free = 0;
+}
+
+void mergeFreeBlocks() {
+    Block* current = freeList;
+    while (current && current->next) {
+        Block* nextBlock = current->next;
+        if (current->free && nextBlock->free) {
+            current->size += sizeof(Block) + nextBlock->size;
+            current->next = nextBlock->next;
+        } else {
+            current = current->next;
+        }
+    }
 }
 
 void* MyMalloc(size_t size) {
@@ -61,4 +76,6 @@ void MyFree(void* ptr) {
 
     Block* block = (Block*)((unsigned char*)ptr - sizeof(Block));
     block->free = 1;
+
+    mergeFreeBlocks();
 }
