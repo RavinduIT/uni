ce2a1df643670b3fdfc812f9670ba9e12d49db43
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2025-01-21 14:16:59 +0530

    done

---
+++ note.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "first line\n";
+    cout << 3 + 3 << endl ;
+    cout << "Hutta" << endl;
+    cout << "I'm \"Ravindu\"";
+    
+    //comment
+    /*comment*/
+    
+}
