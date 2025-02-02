3848a13cdf4c78d216db1fede259df1c53ef23ce
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2025-02-02 16:40:30 +0530

    done

--- note.cpp
+++ note.cpp
@@ -1,66 +1,73 @@
 #include <iostream>
-#include <string> //for string class
-#include <cmath> //for math functions
+#include <string> // For string class
+#include <cmath>  // For math functions
+#include <vector> // For vector (dynamic array)
 
 using namespace std;
 
-int main () {
+int main() {
     int x;
 
-    cout << "first line\n";
-    cout << 3 + 3 << endl ;
+    cout << "First line\n";
+    cout << 3 + 3 << endl;
     cout << "Hutta" << endl;
-    cout << "I'm \"Ravindu\"";
-    cout << "I'm \'Ravindu\'";
-    
-    //comment
-    /*comment*/
+    cout << "I'm \"Ravindu\"" << endl;
+    cout << "I'm \'Ravindu\'" << endl;
+
+    // Comment examples
+    // Single-line comment
+    /* Multi-line comment */
+
+    // Input integer
     cout << "Enter a number: ";
     cin >> x;
     cout << "You entered: " << x << endl;
 
+    // String usage
     string name = "Ravindu";
     cout << name << endl;
 
-    // string concatenation
+    // String concatenation
     string firstName = "Ravindu";
     string lastName = "Lakshan";
     string fullName = firstName + " " + lastName;
-    //string fullName = firstName.append(lastName);
     cout << fullName << endl;
 
-    //string length
+    // String length
     cout << fullName.length() << endl;
-    //cout << fullName.size() << endl;
 
-    //accessing string elements
+    // Accessing string elements
     cout << fullName[0] << endl;
 
-    //changing string elements
+    // Changing string elements
     fullName[0] = 'K';
     cout << fullName << endl;
 
-    //user input string 
+    // User input string
     string name1;
     cout << "Enter your name: ";
-    cin >> name1; //only get the first word
-    getline(cin, name1); //get the whole line
+    cin >> name1;          // Only gets the first word
+    cin.ignore();          // Clear the newline character
+    getline(cin, name1);   // Get the whole line
     cout << "Your name is: " << name1 << endl;
 
-    //Math
-    cout << max(5,10) << endl;   // max out of two numbers
-    cout << min(5,10) << endl;   // min out of two numbers
-    cout << sqrt(64) << endl;    // square root
-    cout << round(2.6) << endl;  // round off
-    cout << log(2) << endl;      // logarithm
+    // Math functions
+    cout << max(5, 10) << endl;   // Max of two numbers
+    cout << min(5, 10) << endl;   // Min of two numbers
+    cout << sqrt(64) << endl;    // Square root
+    cout << round(2.6) << endl;  // Round off
+    cout << log(2) << endl;      // Logarithm
 
+    // Ternary operator
     int time = 20;
     string result = (time < 18) ? "Good day." : "Good evening.";
-    cout << result;
+    cout << result << endl;
 
-    
-
+    // For-each loop
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    for (int i : myNumbers) {
+        cout << i << "\n";
+    }
 
     return 0;
-    
 }
