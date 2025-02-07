d979fd053fedaecf98b5384e5ecb337cae9291e9
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2025-02-07 12:26:31 +0530

    done

---
+++ note.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form method="getinput">
+        <input type="text" name="name">
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+        $var = $_GET['name'];
+        echo "Your name is ".name.".";
+    ?>
+
+
+</body>
+</html>
